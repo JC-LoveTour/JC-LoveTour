b356f12691f133660d25d06314e44115dbfb9e51
siseo123 <tpdlf9801@gmail.com>
2023-09-12 05:20:42 +0900

    경로 작업

---
+++ front/src/pages/CustomerServicePages/announceBoard.jsx
@@ -0,0 +1,14 @@
+import React, { useCallback, useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import AnnounceBoardView from '../../components/Board/CustomerService/announceBoardView';
+
+// import { TextArea } from 'react-bootstrap';
+
+function announceBoard() {
+  return (
+    <>
+      <AnnounceBoardView />
+    </>
+  );
+}
+export default announceBoard;
