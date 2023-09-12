84b63f7fec4d8d8ed8046349a01d437e5a3e212a
siseo123 <132524740+siseo123@users.noreply.github.com>
2023-09-12 19:20:32 +0900

    테스트입니다.
    폭탄 받아라

--- front/src/pages/CustomerServicePages/announceBoard.jsx
+++ front/src/pages/CustomerServicePages/announceBoard.jsx
@@ -1,7 +1,7 @@
 import React, { useCallback, useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import AnnounceBoardView from '../../components/Board/CustomerService/announceBoardView';
-
+// 테스트
 // import { TextArea } from 'react-bootstrap';
 
 function announceBoard() {
